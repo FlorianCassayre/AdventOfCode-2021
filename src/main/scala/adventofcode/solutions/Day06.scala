3117da08f38c7a355da6531dc57c4b026b5954b8
Florian Cassayre <florian.cassayre@gmail.com>
2021-12-06 06:39:49 +0100

    Improved day 06

--- src/main/scala/adventofcode/solutions/Day06.scala
+++ src/main/scala/adventofcode/solutions/Day06.scala
@@ -4,16 +4,13 @@ import adventofcode.Definitions.*
 
 @main def Day06 = Day(6) { (input, part) =>
 
-  val initial = input.split(",").map(BigInt.apply).groupBy(identity).view.mapValues(s => BigInt(s.size)).toSeq
+  val `6` = 6
+  val `8` = 8
 
-  def simulate(state: Seq[(BigInt, BigInt)], i: Int): BigInt =
-    if i > 0 then
-      simulate(
-        state.flatMap((v, n) => if v > 0 then Seq((v - 1, n)) else Seq((BigInt(6), n), (BigInt(8), n)))
-          .groupBy((v, _) => v).view.mapValues(_.map((_, n) => n).sum).toSeq,
-        i - 1)
-    else
-      state.map((_, n) => n).sum
+  val initial = input.split(",").map(_.toInt).foldLeft(IndexedSeq.fill(`8` + 1)(BigInt(0)))((array, v) => array.updated(v, array(v) + 1))
+
+  def simulate(state: IndexedSeq[BigInt], i: Int): BigInt =
+    if i > 0 then simulate((state.tail :+ state.head).updated(`6`, state(`6` + 1) + state.head), i - 1) else state.sum
 
   part(1) = simulate(initial, 80)
 
