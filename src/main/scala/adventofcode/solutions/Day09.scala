a0b661fa66386691eefde9cba024844f261efd8c
Florian Cassayre <florian.cassayre@gmail.com>
2021-12-09 07:02:07 +0100

    Improved day 09

--- src/main/scala/adventofcode/solutions/Day09.scala
+++ src/main/scala/adventofcode/solutions/Day09.scala
@@ -10,24 +10,24 @@ import scala.util.chaining.*
   type Position = (Int, Int)
 
   extension (m: map.type)
-    def apply(p: Position): Int = p.pipe((i, j) => m(i)(j))
-    def has(p: Position): Boolean = p.pipe((i, j) => m.indices.contains(i) && m(i).indices.contains(j))
+    def apply(p: Position): Int = p.pipe(m(_)(_))
+    def has(p: Position): Boolean = p.pipe(m.indices.contains(_) && m.head.indices.contains(_))
     def adjacentOf(p: Position): Seq[Position] =
       Seq((-1, 0), (1, 0), (0, -1), (0, 1)).map((i1, j1) => p.pipe((i, j) => (i + i1, j + j1))).filter(m.has)
 
   val sources =
     for
       i <- map.indices
-      j <- map(i).indices
+      j <- map.head.indices
       p = (i, j)
-      if map.adjacentOf(p).forall(q => map(p) < map(q))
+      if map.adjacentOf(p).forall(map(p) < map(_))
     yield p
 
   part(1) = sources.map(map(_) + 1).sum
 
   def bfs(visit: Set[Position], seen: Set[Position]): Set[Position] =
     if visit.nonEmpty then
-      val next = visit.flatMap(p => map.adjacentOf(p).filter(map(_) < 9).filterNot(seen.contains))
+      val next = visit.flatMap(map.adjacentOf(_).filter(map(_) < 9).filterNot(seen.contains))
       bfs(next, seen ++ next)
     else
       seen
