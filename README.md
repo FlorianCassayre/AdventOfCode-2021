17ac614f00f105ad60121f4a838e6bdaae5ec53c
Florian Cassayre <florian.cassayre@gmail.com>
2021-12-11 06:54:55 +0100

    Solved day 11

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ _My solutions to the 2021 edition of [Advent of Code](https://adventofcode.com/2
   | **[08](https://adventofcode.com/2021/day/8)** | [solution](src/main/scala/adventofcode/solutions/Day08.scala) | `00:13:22 / 3326` | `00:21:17 / 106` |
   | **[09](https://adventofcode.com/2021/day/9)** | [solution](src/main/scala/adventofcode/solutions/Day09.scala) | `00:05:45 / 456` | `00:19:51 / 716` |
   | **[10](https://adventofcode.com/2021/day/10)** | [solution](src/main/scala/adventofcode/solutions/Day10.scala) | `00:07:37 / 826` | `00:26:13 / 2916` |
-  | **[11](https://adventofcode.com/2021/day/11)** | [](src/main/scala/adventofcode/solutions/Day11.scala) |  |  |
+  | **[11](https://adventofcode.com/2021/day/11)** | [solution](src/main/scala/adventofcode/solutions/Day11.scala) | `00:21:48 / 1483` | `00:23:34 / 1315` |
   | **[12](https://adventofcode.com/2021/day/12)** | [](src/main/scala/adventofcode/solutions/Day12.scala) |  |  |
   | **[13](https://adventofcode.com/2021/day/13)** | [](src/main/scala/adventofcode/solutions/Day13.scala) |  |  |
   | **[14](https://adventofcode.com/2021/day/14)** | [](src/main/scala/adventofcode/solutions/Day14.scala) |  |  |
---
+++ src/main/scala/adventofcode/solutions/Day11.scala
@@ -0,0 +1,59 @@
+package adventofcode.solutions
+
+import adventofcode.Definitions.*
+
+import scala.collection.View
+import scala.util.chaining.*
+
+@main def Day11 = Day(11) { (input, part) =>
+
+  type State = IndexedSeq[IndexedSeq[Int]]
+
+  val initial = input.toLines.map(_.map(_.asDigit))
+
+  def next(state: State): (State, Int) =
+    def flash(state: State, flashed: Set[(Int, Int)]): (State, Int) =
+      val toFlash =
+        for
+          i <- state.indices
+          j <- state(i).indices
+          t = (i, j)
+          if state(i)(j) > 9 && !flashed.contains(t)
+        yield t
+      if toFlash.nonEmpty then
+        val newState = toFlash.foldLeft(state) { case (state, (i, j)) =>
+          val r = -1 to 1
+          val adjacent =
+            for
+              id <- r
+              jd <- r
+              i1 = i + id
+              j1 = j + jd
+              if state.indices.contains(i1) && state(i1).indices.contains(j1)
+            yield (i1, j1)
+          adjacent.foldLeft(state) { case (state, (i, j)) => state.updated(i, state(i).updated(j, state(i)(j) + 1)) }
+        }
+        flash(newState, flashed ++ toFlash)
+      else
+        (state.zipWithIndex.map((row, i) => row.zipWithIndex.map((v, j) => if flashed.contains((i, j)) then 0 else v)), flashed.size)
+    flash(state.map(_.map(_ + 1)), Set.empty)
+
+  def count(state: State, n: Int, f: Int): Int =
+    if n > 0 then
+      val (s, f1) = next(state)
+      count(s, n - 1, f + f1)
+    else
+      f
+
+  part(1) = count(initial, 100, 0)
+
+  def sync(state: State, n: Int): Int =
+    if state.forall(_.forall(_ == 0)) then
+      n
+    else
+      val (s, _) = next(state)
+      sync(s, n + 1)
+
+  part(2) = sync(initial, 0)
+
+}
