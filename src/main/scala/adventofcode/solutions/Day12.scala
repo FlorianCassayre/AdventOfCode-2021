5ce83d277be22ef31e60023ba43863ec007a7235
Florian Cassayre <florian.cassayre@gmail.com>
2021-12-12 10:17:57 +0100

    Improved day 12

--- src/main/scala/adventofcode/solutions/Day12.scala
+++ src/main/scala/adventofcode/solutions/Day12.scala
@@ -1,28 +1,25 @@
 package adventofcode.solutions
 
 import adventofcode.Definitions.*
+import scala.util.chaining.*
 
 @main def Day12 = Day(12) { (input, part) =>
 
-  val edges = input.toLines.map { case s"$l-$r" => l -> r }
-  val graph = (edges ++ edges.map(_.swap)).groupBy((l, _) => l).view.mapValues(_.map((_, r) => r).toSet).toMap
+  val graph = input.toLines.flatMap { case s"$l-$r" => Seq(l -> r, r -> l) }.groupMap((l, _) => l)((_, r) => r)
 
   def isLarge(s: String): Boolean = s.forall(_.isUpper)
 
   val (start, end) = ("start", "end")
 
-  def count(f: (String, Seq[String]) => Boolean, path: Seq[String] = Seq(start)): Int =
+  def count(f: Seq[String] => Boolean, path: Seq[String] = Seq(start)): Int =
     val s = path.head
     if s == end then
       1
     else
-      graph(s).filter(t => t != start && (isLarge(t) || !path.contains(t) || f(t, path))).toSeq.map(t => count(f, t +: path)).sum
+      graph(s).filter(t => t != start && (isLarge(t) || !path.contains(t) || f(path))).map(t => count(f, t +: path)).sum
 
-  part(1) = count((_, _) => false)
+  part(1) = count(_ => false)
 
-  part(2) = count { (s, p) =>
-    val d = p.filterNot(isLarge)
-    d.distinct.size == d.size
-  }
+  part(2) = count(_.filterNot(isLarge).pipe(d => d.distinct.size == d.size))
 
 }
