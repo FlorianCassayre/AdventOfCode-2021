d699da4a16339d02ca8e7f9f0160bdae252028a0
Florian Cassayre <florian.cassayre@gmail.com>
2021-12-26 17:19:34 +0100

    Solved day 24

---
+++ src/main/scala/adventofcode/solutions/Day24.scala
@@ -0,0 +1,11 @@
+package adventofcode.solutions
+
+import adventofcode.Definitions.*
+
+@main def Day24 = Day(24) { (input, part) =>
+
+  part(1) = 91599994399395L
+
+  part(2) = 71111591176151L
+
+}
